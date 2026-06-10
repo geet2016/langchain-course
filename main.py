d21e991805506fcdd9711f4b1dc4eaa0028be197
geet2016 <gchdhr02@gmail.com>
2026-06-10 22:34:13 +0530

    env setup

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+
+from dotenv import dotenv_values, load_dotenv
+
+
+def main():
+    print("Hello from langchain-course!")
+    #load_dotenv(dotenv_path=r"D:\Agentic AI\langchain-course\.env")
+    
+
+
+if __name__ == "__main__":
+    main()
